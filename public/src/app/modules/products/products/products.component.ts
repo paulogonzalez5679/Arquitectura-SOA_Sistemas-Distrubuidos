13eb6b434c1e983dd5a0665d8803f6bc34d62ce1
Paulo Gonzalez <paulomga10@gmail.com>
2020-12-12 16:47:59 -0500

    proxys arreglados, se recupera los 2 servicios

--- public/src/app/modules/products/products/products.component.ts
+++ public/src/app/modules/products/products/products.component.ts
@@ -3,6 +3,7 @@ import swal from "sweetalert2";
 import * as firebase from "firebase/app";
 import { ProductService } from "../../../services/product/product.service";
 import { UsersService } from "app/services/users/users.service";
+import { DataService } from "app/services/data.service";
 
 
 declare var $: any;
@@ -17,6 +18,12 @@ export interface DataTable {
   styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent implements OnInit {
+
+  public comida: Comida[];
+  // public musica: Musica[];
+  public busqueda: string = "";
+  public busqueda2: string = "";
+  public dataTable2: DataTable;
   public dataTable: DataTable;
   public product: Decoration;
   public category: Category;
@@ -26,6 +33,7 @@ export class ProductsComponent implements OnInit {
   public documentId = null;
   public isEdit = false;
   public tablaDatos;
+  public tablaDatos2;
   public imageFile: any;
   public imageSrc: any;
   public disabled: boolean = false;
@@ -34,14 +42,23 @@ export class ProductsComponent implements OnInit {
   public stateSw = false;
   public usuario: Users;
   public infoUser: Users;
-    public user: Users;
-  constructor(private productService: ProductService, private usersService: UsersService,) { }
+  public user: Users;
+  public comidaUser: Comida;
+  public food: Food;
+
+
+  constructor(private productService: ProductService, private usersService: UsersService,
+    
+    private dataService: DataService) { }
 
   ngOnInit(): void {
+ 
     this.infoUser = JSON.parse(localStorage.getItem("infoUser"));
     this.user = {};
-    this.product= JSON.parse(localStorage.getItem("product"));
-    this.product={};
+    this.product = JSON.parse(localStorage.getItem("product"));
+    this.product = {};
+    this.comida = JSON.parse(localStorage.getItem("comida"))
+    this.comida= [];
 
     this.dataTable = {
       headerRow: [
@@ -55,26 +72,37 @@ export class ProductsComponent implements OnInit {
       ],
       dataRows: [],
     };
+
+    this.dataTable2 = {
+      headerRow: [
+        "nombre",
+        "Receta",
+        "Imagen",
+        "Energia Calorica",
+        "Proteina",
+        "Fibra",
+        "Seleccionar",
+      ],
+      dataRows: [],
+    };
     this.tablaDatos = $("#datatablesProduct").DataTable({});
+    this.tablaDatos2 = $("#datatablesProduct2").DataTable({});
     this.getProducts();
     this.getUsers();
-
-
-
   }
   async getUsers() {
     (await this.usersService.getUserByEmails(this.infoUser.user_email)).subscribe((user) => {
-        console.log(user);
-        this.user = user;
-        console.log(this.user)
+      console.log(user);
+      this.user = user;
+      console.log(this.user)
     });
-}
+  }
   async getProducts() {
     await this.productService.getProducts('all').subscribe((prductSnapshot) => {
       this.arrayproduct = [];
       console.log(this.arrayproduct);
       prductSnapshot.forEach((categoryData) => {
-        
+
         this.arrayproduct.push(categoryData.payload.doc.data());
         // console.log(prductSnapshot.length + '' + this.arrayproduct.length);
         if (prductSnapshot.length == this.arrayproduct.length) {
@@ -83,7 +111,18 @@ export class ProductsComponent implements OnInit {
       });
     });
   }
-  
+  consultar() {
+    this.dataService.sendGetRequest(this.busqueda).subscribe((data: any[]) => {
+      console.log(data['hints']);
+      this.comida = data['hints'];
+    })
+  }
+  consultar2() {
+    this.dataService.getMusica(this.busqueda2).subscribe((data2: any[]) => {
+      console.log(data2);
+      // this.comida = data['hints'];
+    })
+  }
   initDataTable() {
     let aaa = this.tablaDatos;
     $("#datatablesProduct").DataTable().destroy();
@@ -116,86 +155,58 @@ export class ProductsComponent implements OnInit {
      * @param decoracion
      * @param valid
      */
-  estadoNuevo(e) {
-
-
+  estadoNuevo(e, product) {
     if (e) {
+      console.log(e, product);
+      this.product = product;
       console.log(this.product);
-        this.productService.createDecoracion(this.product, this.infoUser).then(() => {
-          this.product = {
-            id: "",
-            nombre: "",
-            descripcion: "",
-            precio: "",
-            imgen: "",
-          };
-        });
+      this.productService.createDecoracion(this.product, this.infoUser).then(() => {
+        this.product = {
+          id: "",
+          nombre: "",
+          descripcion: "",
+          precio: "",
+          imgen: "",
+        };
+      });
+    }
+  }
+  estadoNuevo2(e, comida) {
+    if (e) {
+      console.log(e, comida);
+      this.comidaUser = comida;
+      console.log( this.comidaUser);
+      this.productService.createComida(this.comidaUser, this.infoUser).then(() => {
+        this.comidaUser = {
+          food: this.comidaUser.food
+        };
+      });
     }
   }
 
-  // estadoLoMasVendido(e) {
-  //   var cont2 = 0;
-  //   var productAux2 = this.product;
+  initDataTable2() {
+    let aaa = this.tablaDatos;
+    $("#datatablesProduct2").DataTable().destroy();
+    setTimeout(function () {
+      /*
+       * Opciones del datatable
+       */
+      aaa = $("#datatablesProduct2").DataTable({
+        paging: true,
+        ordering: true,
+        info: true,
+        pagingType: "full_numbers",
+        lengthMenu: [
+          [10, 25, 50, -1],
+          [10, 25, 50, "All"],
+        ],
+        responsive: true,
+        language: {
+          search: "",
+          searchPlaceholder: "Buscar",
+        },
+      });
+    }, 10);
+  }
 
-  //   console.log(e.currentValue)
-
-  //   this.arrayproduct.forEach((element) => {
-  //     if (this.product.pro_categoria == element.pro_categoria) {
-  //       if (element.pro_estado_novedad == true) {
-  //         cont2++;
-  //         console.log(
-  //           cont2 + ".- " + element.pro_estado_novedad + ".- " + element.pro_nombre
-  //         );
-  //       }
-
-
-  //     }
-  //   });
-  //   this.cont2 = cont2;
-
-  //   if (cont2 > 3) {
-  //     swal(
-  //       "Error",
-  //       "NO SE PUEDE AGREGAR MAS PRODUCTOS NUEVOS EN LA CATEGORÍA: " + this.product.pro_categoria,
-  //       "error"
-  //     );
-  //     this.product.pro_estado_novedad = false;
-  //     $('#modalProduct').modal('hide');
-  //     this.product = {};
-  //     // this.product = productAux;
-  //     // this.selectCategory(this.product);
-  //   } else {
-  //     console.log(this.product);
-  //     if (this.product.pro_nombre != '') {
-  //       this.productService.updateProductState(this.product);
-  //     }
-  //   }
-  //   console.log(
-  //     "*** TOTAL PARA: => *** " + this.product.pro_categoria + " = " + cont2
-  //   );
-  // }
-
-  // estadoDecuento(eD) {
-  //   var cont3 = 0;
-
-  //   this.product.pro_estado_descuento = eD.currentValue;
-  //   this.arrayproduct.forEach((element) => {
-  //     if (this.product.pro_categoria == element.pro_categoria) {
-  //       console.log(element.pro_estado_descuento);
-  //       if (element.pro_estado_descuento == true) {
-  //         cont3++;
-  //         console.log("hola");
-  //         console.log(cont3);
-  //       }
-  //       if (cont3 == 3) {
-  //         swal(
-  //           "Error",
-  //           "NO SE PUEDE AGREGAR MAS PRODUCTOS NUEVOS EN ESTA CATEGORÍA",
-  //           "error"
-  //         );
-  //         this.product.pro_estado_descuento = false;
-  //       }
-  //     }
-  //   });
-  // }
 }
---
+++ public/src/app/services/data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  
+  private REST_API_SERVER = "/getFood/";
+  private REST_API_SERVER2 = "/getMusica/";
+
+  constructor(private httpClient: HttpClient) { }
+  
+  public sendGetRequest(busqueda: string){
+    return this.httpClient.get(this.REST_API_SERVER+''+busqueda);
+  }
+
+  public getMusica(busqueda2: string){
+    return this.httpClient.get(this.REST_API_SERVER2+''+busqueda2);
+  }
+
+  
+}
