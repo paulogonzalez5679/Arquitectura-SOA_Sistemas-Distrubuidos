8607431422881113a19372813595a0eeb9713282
Paulo Gonzalez <paulomga10@gmail.com>
2020-12-12 19:03:30 -0500

    comentarios

--- public/src/app/interfaces/musica.ts
+++ public/src/app/interfaces/musica.ts
@@ -2,7 +2,7 @@ declare interface Musica {
     track?: Track;    
 }
 declare interface Track{
-  subtitle?:string;
-  title?: string;
-  url?:string
+  subtitle?:string,
+  title?: string,
+  url?:string,
 }
